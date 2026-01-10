212e0395a496f3f6bf01d3162f777746c2a5c92d
tinhtam221986 <tinhtam221986@gmail.com>
2026-01-10 07:34:32 +0700

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import BottomNav from '@/components/navigation/BottomNav';
 
 interface AppShellProps {
   children: React.ReactNode;
@@ -9,9 +10,16 @@ interface AppShellProps {
 export default function AppShell({ children }: AppShellProps) {
   return (
     <div className="relative min-h-screen flex flex-col bg-black overflow-x-hidden">
-      {/* Khu vực hiển thị nội dung chính */}
-      <div className="flex-1 w-full max-w-md mx-auto relative pb-20">
+      {/* Khu vực nội dung chính */}
+      <div className="flex-1 w-full max-w-md mx-auto relative">
         {children}
+      </div>
+      
+      {/* Thanh điều hướng 5 nút luôn ở dưới cùng */}
+      <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-center">
+        <div className="w-full max-w-md">
+          <BottomNav />
+        </div>
       </div>
     </div>
   );
